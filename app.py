63cd1d6896017ea88236fe1160409aaa97ef8fce
Shahid-khan015 <khan.shahidljpit2020@gmail.com>
2024-11-25 22:52:00 +0530

    first commit

--- app.py
+++ app.py
@@ -1,18 +1,4 @@
-from flask import Flask, jsonify
-  from website import create_app
-app = Flask(__name__)
-
-@app.route('/troubleshoot', methods=['GET'])
-def troubleshoot():
-    try:
-        # Simulate some processing
-        large_array = [i for i in range(1_000_000)]  # Adjust as necessary
-        return jsonify({"message": "Troubleshooting function executed successfully!"})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-
-  
+from website import create_app
 
 app = create_app()
 application = app

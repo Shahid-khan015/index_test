1b8093ee094a00f0f42827b409820182b8099460
Shahid-khan015 <khan.shahidljpit2020@gmail.com>
2024-11-26 14:04:10 +0530

    Added

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from website import create_app
+from flask import Flask, jsonify, send_file
+import os
 
-app = create_app()
-application = app
+base_folder = "numbers"
+list_numbers = []
+for l in os.listdir(base_folder):
+    list_numbers.append(f"{base_folder}/{l}")
 
-# Remove debug mode for production
+app = Flask(__name__)
+
+
+@app.route("/<int:videoID>")
+def videoID(videoID):
+
+    print(videoID)
+
+    return send_file(f"{list_numbers[videoID]}", mimetype='video/mp4')
+
+
+
+
+@app.route("/videos")
+def video_list():
+    return jsonify({
+        "length": len(list_numbers),
+        "urls": list_numbers
+    })
+
+
 if __name__ == '__main__':
     app.run()
